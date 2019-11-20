3e007cbc0c9bdd06efa88a349aaa26455ffc0c80
systemlhm <systemlhm@qq.com>
2019-11-20 18:00:00 +0800

    这是写了配置的提交

--- src/test/java/com/sy/test/Test22.java
+++ src/test/java/com/sy/test/Test22.java
@@ -1,5 +1,9 @@
 package com.sy.test;
-
+/**
+ * git进行远程提交的时候，必须先提交的本地库中才行，不能直接往远程库提交。
+ * @author Administrator
+ *
+ */
 public class Test22 {
 	public static void main(String[] args) {
 		System.out.println();
